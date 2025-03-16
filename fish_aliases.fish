506f85ffa58aa9433837be31acb413e59bdcbe4c
agus <33077305+agusc01@users.noreply.github.com>
2025-03-17 09:19:48 +1100

    ADDED slock

--- fish_aliases.fish
+++ fish_aliases.fish
@@ -56,3 +56,4 @@ alias udmenu='cd $HOME/.config/dmenu && sudo rm config.h && sudo make clean inst
 alias udwm='cd $HOME/.config/dwm && sudo rm config.h && sudo make clean install'
 alias udbar='cd $HOME/.config/dbar && sudo rm config.h && sudo make clean install'
 alias ust='cd $HOME/.config/st && sudo rm config.h && sudo make clean install'
+alias uslock='cd $HOME/.config/slock && sudo rm config.h && sudo make clean install'
