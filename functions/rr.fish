594d652a8356dce19bae1cdc2f04129bc3db8ea4
agus <33077305+agusc01@users.noreply.github.com>
2025-03-30 10:15:28 -0300

    fixed function rr

--- functions/rr.fish
+++ functions/rr.fish
@@ -1,14 +1,12 @@
-# something like ranger
 function rr
-    set command (ls -l | grep -v "^d" | grep -v "^l" | awk "{print $9}" | fzf --height 90% --preview "echo {} | awk '\''{for(i=9;i<=NF;i++) printf \$i (i<NF?\" \":\"\n\");}'\'' | xargs -I {} batcat --style=numbers --color=always {}")
-    set current_command (echo $command | awk '{print $9}')
+    set precommand (ls -la | sed '1d' | grep -v '^d' | grep -v '^l')
+    set command (printf "%s\n" $precommand | fzf --height 90% --preview "echo {} | awk '{print \$9}' | xargs -I {} batcat --style=numbers --color=always {}")
 
-    if test -n "$current_command"
+    if test -n "$command"
         echo -e "\nnvim $command\n"
-        echo -e "eval:nvim $current_command\n"
-        eval "nvim $current_command"
+        echo -e "eval:nvim $command\n"
+        eval "nvim $command"
     else
-        echo "Aborted !"
+        echo "Aborted!"
     end
-
 end
