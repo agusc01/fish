f7decfb82cc995f7fec591055eb24c4d39cebb2b
agus <33077305+agusc01@users.noreply.github.com>
2025-05-01 09:22:23 -0300

    fixed bug en rr.fish function

--- functions/rr.fish
+++ functions/rr.fish
@@ -2,10 +2,12 @@ function rr
     set precommand (ls -la | sed '1d' | grep -v '^d' | grep -v '^l')
     set command (printf "%s\n" $precommand | fzf --height 90% --preview "echo {} | awk '{print \$9}' | xargs -I {} batcat --style=numbers --color=always {}")
 
-    if test -n "$command"
-        echo -e "\nnvim $command\n"
-        echo -e "eval:nvim $command\n"
-        eval "nvim $command"
+    set file (echo "$command" | awk '{print $9}')
+
+    if test -n "$file"
+        echo -e "\nnvim $file\n"
+        echo -e "eval:nvim $file\n"
+        eval "nvim $file"
     else
         echo "Aborted!"
     end
