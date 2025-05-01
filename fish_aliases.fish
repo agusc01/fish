7c2c7aa792e86373693c7216eabc489697e75f1a
agus <33077305+agusc01@users.noreply.github.com>
2025-05-01 19:15:45 -0300

    dbar scripts aliases

--- fish_aliases.fish
+++ fish_aliases.fish
@@ -1,3 +1,6 @@
+# ======================================================
+# globals
+# ======================================================
 alias apps='bash $bash_scripts/globals/apps'
 alias falias='bash $bash_scripts/globals/falias'
 alias ffind='bash $bash_scripts/globals/ffind'
@@ -11,3 +14,15 @@ alias setsound='bash $bash_scripts/globals/setsound'
 alias b0='bash $bash_scripts/globals/b0'
 alias b1='bash $bash_scripts/globals/b1'
 alias wifii='bash $bash_scripts/globals/wifii'
+
+# ======================================================
+# dbar
+# ======================================================
+alias dbar_battery='bash $bash_scripts/dbar/battery'
+alias dbar_brightness='bash $bash_scripts/dbar/brightness'
+alias dbar_cpu='bash $bash_scripts/dbar/cpu'
+alias dbar_date='bash $bash_scripts/dbar/date'
+alias dbar_memory='bash $bash_scripts/dbar/memory'
+alias dbar_thermal='bash $bash_scripts/dbar/thermal'
+alias dbar_volume='bash $bash_scripts/dbar/volume'
+alias dbar_wifi='bash $bash_scripts/dbar/wifi'
