03b3f0745944843b6a7310609d1cd2a139c276d8
agus <33077305+agusc01@users.noreply.github.com>
2025-05-21 22:29:38 -0300

    script to make fish aliases easier

--- fish_aliases.fish
+++ fish_aliases.fish
@@ -1,41 +1,44 @@
 # ======================================================
 # globals
+# ls .bash_scripts/globals/ | xargs | sed 's/ /\n/g' | awk '{print "alias "$1"=\'bash $bash_scripts/globals/"$1"\'"}' | copyme
 # ======================================================
 alias apps='bash $bash_scripts/globals/apps'
+alias b0='bash $bash_scripts/globals/b0'
+alias b1='bash $bash_scripts/globals/b1'
+alias bluetooth='bash $bash_scripts/globals/bluetooth'
+alias brightness='bash $bash_scripts/globals/brightness'
 alias chistory='bash $bash_scripts/globals/chistory'
+alias dbar='bash $bash_scripts/globals/dbar'
 alias falias='bash $bash_scripts/globals/falias'
 alias ffind='bash $bash_scripts/globals/ffind'
 alias fhistory='bash $bash_scripts/globals/fhistory'
 alias fsignal='bash $bash_scripts/globals/fsignal'
+alias killdbar='bash $bash_scripts/globals/killdbar'
 alias lf='bash $bash_scripts/globals/lf'
+alias msg='bash $bash_scripts/globals/msg'
+alias oonboard='bash $bash_scripts/globals/oonboard'
+alias prompt='bash $bash_scripts/globals/prompt'
 alias refreshdbar='bash $bash_scripts/globals/refreshdbar'
+alias refreshdunst='bash $bash_scripts/globals/refreshdunst'
 alias rmf='bash $bash_scripts/globals/rmf'
+alias rotation='bash $bash_scripts/globals/rotation'
 alias rr='bash $bash_scripts/globals/rr'
-alias setbrightness='bash $bash_scripts/globals/setbrightness'
-alias b0='bash $bash_scripts/globals/b0'
-alias b1='bash $bash_scripts/globals/b1'
-alias setsound='bash $bash_scripts/globals/setsound'
 alias s0='bash $bash_scripts/globals/s0'
 alias s1='bash $bash_scripts/globals/s1'
+alias setbrightness='bash $bash_scripts/globals/setbrightness'
+alias setsound='bash $bash_scripts/globals/setsound'
 alias smute='bash $bash_scripts/globals/smute'
-alias sunmute='bash $bash_scripts/globals/sunmute'
+alias sound='bash $bash_scripts/globals/sound'
 alias stogglemute='bash $bash_scripts/globals/stogglemute'
+alias sunmute='bash $bash_scripts/globals/sunmute'
+alias updates='bash $bash_scripts/globals/updates'
 alias wifi='bash $bash_scripts/globals/wifi'
 alias wifiip='bash $bash_scripts/globals/wifiip'
-alias updates='bash $bash_scripts/globals/updates'
-alias msg='bash $bash_scripts/globals/msg'
-alias prompt='bash $bash_scripts/globals/prompt'
-alias bluetooth='bash $bash_scripts/globals/bluetooth'
-alias oonboard='bash $bash_scripts/globals/oonboard'
-alias refreshdunst='bash $bash_scripts/globals/refreshdunst'
-alias brightness='bash $bash_scripts/globals/brightness'
-alias sound='bash $bash_scripts/globals/sound'
-alias rotation='bash $bash_scripts/globals/rotation'
-alias dbar='bash $bash_scripts/globals/dbar'
-alias killdbar='bash $bash_scripts/globals/killdbar'
+
 
 # ======================================================
 # dbar
+# ls .bash_scripts/dbar/ | xargs | sed 's/ /\n/g' | awk '{print "alias dbar_"$1"=\'bash $bash_scripts/dbar/"$1"\'"}' | copyme
 # ======================================================
 alias dbar_battery='bash $bash_scripts/dbar/battery'
 alias dbar_bluetooth='bash $bash_scripts/dbar/bluetooth'
@@ -50,6 +53,7 @@ alias dbar_wifi='bash $bash_scripts/dbar/wifi'
 
 # ======================================================
 # dshortcuts
+# ls .bash_scripts/dshortcuts/ | xargs | sed 's/ /\n/g' | awk '{print "alias dshortcuts_"$1"=\'bash $bash_scripts/dshortcuts/"$1"\'"}' | copyme
 # ======================================================
 alias dshortcuts_border='bash $bash_scripts/dshortcuts/border'
 alias dshortcuts_brightness='bash $bash_scripts/dshortcuts/brightness'
@@ -58,7 +62,7 @@ alias dshortcuts_firefox-private='bash $bash_scripts/dshortcuts/firefox-private'
 alias dshortcuts_gap='bash $bash_scripts/dshortcuts/gap'
 alias dshortcuts_layout='bash $bash_scripts/dshortcuts/layout'
 alias dshortcuts_power='bash $bash_scripts/dshortcuts/power'
+alias dshortcuts_rotation='bash $bash_scripts/dshortcuts/rotation'
 alias dshortcuts_sound='bash $bash_scripts/dshortcuts/sound'
 alias dshortcuts_surf='bash $bash_scripts/dshortcuts/surf'
 alias dshortcuts_toggle='bash $bash_scripts/dshortcuts/toggle'
-alias dshortcuts_rotation='bash $bash_scripts/dshortcuts/rotation'
